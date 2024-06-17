39abe6ba910a4575a76c2b1c632da1ffbcecdd9f
Michael James <micrjames921@gmail.com>
2024-06-17 11:10:48 -0700

    refactored structure of 'creation operation' tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,10 +4,13 @@ import { container } from "../src/ts/incs";
 describe("The DOM", () => {
    beforeAll(() => {
    });
-   describe("Elements created within a page.", () => {
+   describe("Elements created within a page", () => {
 	  test.todo("Should exist in the page.");
-	  test.todo("Should be appended to the 'container' parent element");
-	  test.todo("Should have the correct attributes.");
-	  test.todo("Should have children appended to those elements.");
+	  describe("With internal structures defined", () => {
+		 test.todo("Should be appended to the 'container' parent element.");
+		 test.todo("Should have the correct attributes.");
+		 test.todo("Should have children appended to the elements.");
+		 test.todo("Should have a text node appended to the element.");
+	  });
    });
 });
